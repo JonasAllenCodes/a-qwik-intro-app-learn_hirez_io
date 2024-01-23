fa183fd9a0dd337d81eb7cf88cf98701f7b5b24b
Jonas Allen <Webmaster@JonasAllen.Codes>
2024-01-23 00:30:27 -0800

    Add Event Handler
    I learned how to add a event handler to a button in the root index.
    I am impressed with the broken up lazy loading of JavaScript. This
    is Qwik! ;)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,14 +4,17 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <>
-      <Misko />
-      <span>
-        Hello World!
-      </span>
+      <Jonas />
+      <div>
+        The workers are comming home!
+      </div>
+      <button onClick$={() => {
+        console.log('Ahoj!')
+      }}>Ahoj!</button>
     </>
   );
 });
 
-export const Misko = component$(() => {
-  return <div>Bla</div>;
+export const Jonas = component$(() => {
+  return <div>Hello, my name is Jonas. Will you pour yourself some tea?</div>;
 })
